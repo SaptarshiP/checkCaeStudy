9cfcb412a3077840b176d49534a8cf4d759d9720
DESKTOP-J05IIIT\SAPTARSHI <|Saptarshi1295@gmail.com>
2022-01-09 11:31:49 +0530

    Added new main java file

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class main{
+
+	public static void main(String args[]){
+	
+		String fname = "Saptarshi";
+		String lname = "Pramanick";
+
+
+		if ( fname.equals( "Saptarshi" ) ){
+			System.out.println("Hello Saptarhi Pramanick");
+		
+		}  
+
+	}
+
+}
+
